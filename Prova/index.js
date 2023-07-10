12fd37244f3f27e2014e363a289deda9ff2ec558
Arthur Santos <110117873+Arthur100santos@users.noreply.github.com>
2023-07-10 18:23:44 -0300

    Add files via upload

---
+++ Prova/index.js
@@ -0,0 +1,71 @@
+
+//QUESTÃO 2:
+// const path = require('node:path'); 
+// path.basename('https://al.senai.br/cursos/tecnico-em-informatica-para-internet/planodecurso.pdf');
+// c
+
+// onsole.log(path.basename)
+
+
+
+//FIM DA QUESTÃO 2
+
+
+
+
+
+
+
+
+
+
+
+
+//QUESTÃO 3
+
+// const url = require('node:url')
+// const myURL =  new URL('https://zoologico.com.br/categorias/insetos.html#borboletas_ao_redor_do_mundo');
+
+// console.log("protocol: ", myURL.protocol,",");
+// console.log("auth: ",myURL.auth,",");
+// console.log("host: ", myURL.host,",");
+// console.log("hostname: ", myURL.hostname,",");
+// console.log("hash: ", myURL.hash),",";
+// console.log("pathname: ", myURL.pathname,",");
+// console.log("pathname: ", myURL.path,",");
+// console.log("pathname: ", myURL.href,",");
+
+//FIM DA QUESTÃO 3
+
+
+
+
+
+
+
+
+
+
+
+
+// QUARTA QUESTÃO 4
+// const fs = require('node:fs')
+
+// fs.rename('nomeVelho.txt', 'nomeNovo.txt', (err) => {
+//   if(err) throw(err);
+
+//   console.log('Renomeado!');
+// }); 
+//FIM DA QUESTÃO 4
+
+
+
+
+
+
+
+
+//QUESTÃO 5
+// const os = require('node:os')
+// console.log(os.cpus())
+//FIM DA QUESTÃO 5
